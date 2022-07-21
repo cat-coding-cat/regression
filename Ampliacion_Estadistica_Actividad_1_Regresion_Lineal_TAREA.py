5961d85ff931dc9066bfe5f7e3811dd374770f3a
ivan-paz <ivnpaz@gmail.com>
2022-07-21 20:03:06 +0200

    cleanning homework 1

--- Ampliacion_Estadistica_Actividad_1_Regresion_Lineal_TAREA.py
+++ Ampliacion_Estadistica_Actividad_1_Regresion_Lineal_TAREA.py
@@ -1,53 +1,93 @@
-#
+#----------------------------------------------------------------------------------------
+# ESTAS SON LAS LIBRERIAS QUE UTILIZAMOS
+# matplotlib es para hacer graficos
+# numpy permite manejar arreglos ( arrays)
+# sklearn contiene herramientas para Machine Learning: Algoritmos, metricas para validar
+# sklearn también contiene datasets
+#----------------------------------------------------------------------------------------
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
-# Load the diabetes dataset
+# Cargamos el dataset diabetes. Tenemos un vector X con distintas variables que medimos de una # persona, y la variable y que nos diría "el grado de diabetes" que tiene
+# Recordemos que la diabetes no es una enfermedad binaria sino que se tiene "en cierto grado"
+
 diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
-#visualizamos X,y
 
+
+#----------------------------------------------------------------------------------------
+#visualizamos X,y para darinos una idea del dataset
+print(diabetes_X)
+print(diabetes_y)
+
+#### Puedo seleccionar sólo un feature (una veriable del vector X)
+
+#diabetes_X = diabetes_X[:, np.newaxis, 0] # Cojo la 0
+#diabetes_X = diabetes_X[:, np.newaxis, 4]  # Cojo la 4
+#
 print(diabetes_X)
 
-### Use only one feature
-# 
-# 1. Probar distintos features (variables) y ver cual es la que produce los mejores resultados.
-# 2. Utilizar el conjunto completo de features e imprimir el resultado
+print(len(diabetes_X))
 
-diabetes_X = diabetes_X[:, np.newaxis, 2]
 
-print(diabetes_X)
+#----------------------------------------------------------------------------------------
+#  TAREA 
+## 1. Probar distintos features (variables) y ver cual es la que produce los mejores resultados.
+## 2. Utilizar el conjunto completo de features e imprimir el resultado 
+#     (recuerda que para ello debes desactivar la grafica solo estamos graficando abajo 2 dimensiones)
 
-## Split the data into training/testing sets
+
+#----------------------------------------------------------------------------------------
+# PARA ENTRENAR EL MODELO Y TENER CON QUE PROBARLO LO SEPARAMOS EN train y test
+
+### Split the data into training/testing sets
+# Cojo 20 datos
 diabetes_X_train = diabetes_X[:-20]
 diabetes_X_test = diabetes_X[-20:]
 
-## Split the targets into training/testing sets
+# Cojo 20 datos
+### Split the targets into training/testing sets
 diabetes_y_train = diabetes_y[:-20]
 diabetes_y_test = diabetes_y[-20:]
-#
-## Create linear regression object
-regr = linear_model.LinearRegression()
-#
-## Train the model using the training sets
-regr.fit(diabetes_X_train, diabetes_y_train)
+#----------------------------------------------------------------------------------------
 
-## Make predictions using the testing set
-diabetes_y_pred = regr.predict(diabetes_X_test)
 
-## The coefficients
-print( "Coefficients: \n", regr.coef_ )
-## The mean squared error
+
+
+
+#----------------------------------------------------------------------------------------
+#    ENTRENAMOS NUESTRO MODELO DE MACHINE LEARNING
+
+##  1. Creamos el modelo: the linear regression object
+regression = linear_model.LinearRegression()
+
+
+### 2. Entrenamos (.fit) el modelo usando los training sets
+regression.fit(diabetes_X_train, diabetes_y_train)
+
+### 3. Hacemos predicciones y evaluamos el modelo usando el testing set
+diabetes_y_pred = regression.predict(diabetes_X_test)
+
+
+#----------------------------------------------------------------------------------------
+# IMPRIMIMOS LOS RESULTADOS!!
+### The coefficients
+# si utilizamos una sola variable tendremos 1 solo coeficiente
+# si utilizamos todo el dataset tendremos 10
+print( "Coefficients: \n", regression.coef_ )
+
+# Termino independiente en el modelo lineal
+print( "Intercept: \n", regression.intercept_ )
+
+### The mean squared error
 print( "Mean squared error: %.2f" % mean_squared_error( diabetes_y_test, diabetes_y_pred ) )
-## The coefficient of determination: 1 is perfect prediction
-#print("Coefficient of determination: %.2f" % r2_score(diabetes_y_test, diabetes_y_pred))
+
 #
-## Plot outputs
-plt.scatter(diabetes_X_test, diabetes_y_test, color="black")
-plt.plot(diabetes_X_test, diabetes_y_pred, color="blue", linewidth=3)
-#
-plt.xticks(())
-plt.yticks(())
-#
-plt.show()
+### Plot el modelo (la recta) y los datos originales
+#plt.ylabel('y')
+#plt.scatter(diabetes_X_test, diabetes_y_test, color="black")
+#plt.ylabel('y')
+#plt.plot(diabetes_X_test, diabetes_y_pred, color="blue", linewidth=3)
+#plt.xlabel('x')
+#plt.show()
